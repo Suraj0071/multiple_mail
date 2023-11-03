58e0dce2ec75c17c00ac4497f5b79b605c995eef
Imran Shaikh <80340473+imarb51@users.noreply.github.com>
2023-11-03 17:08:46 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,126 +1,74 @@
+import pyautogui
+from time import sleep
+
+# pyautogui.click(1075,1051)
+# sleep(2)
+# pyautogui.click(998,601)
+# pyautogui.hotkey("ctrl","t")
+# pyautogui.write("https://www.tiktok.com/en/")
+# first import the module 
+import webbrowser 
+  
+# then make a url variable 
+url = "https://www.tiktok.com/en/"
+  
+# then call the default open method described above 
+webbrowser.open(url)
+
+sleep(2)
+
+
+pyautogui.hotkey("enter")
+sleep(10)
+pyautogui.click(1715,180)
+sleep(10)
+pyautogui.click(1056,887)
+sleep(8)
+pyautogui.click(933,548)
+sleep(8)
+
 import csv
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.action_chains import ActionChains
 
 
+# Specify the path to your CSV file
+csv_file = 'csv_credentials.csv'
 
-def main():
-    # Name of the CSV file
-    csv_file = "user_credentials.csv"
+# Initialize empty variables for email and password
+em = None
+ps = None
 
+# Open the CSV file for reading
+with open(csv_file, mode='r', newline='') as file:
 
-    # # Open the CSV file for reading
-    # with open(csv_file, mode='r') as file:
-    #     reader = csv.DictReader(file)
-        
-    #     # Read the first row from the CSV
-    #     first_row = next(reader)
+    reader = csv.DictReader(file)
     
-    # # Access and print the first row's data
-    # email = first_row['Email']
-    # password = first_row['Password']
-    # print(f"First row - Email: {email}, Password: {password}")
-    # Initialize empty lists to store email and password
-    emails = []
-    passwords = []
-
-    # Open the CSV file for reading
-    with open(csv_file, mode='r') as file:
-        reader = csv.DictReader(file)
-        
-        # Read and store data from the CSV
-        for row in reader:
-            emails.append(row['Email'])
-            passwords.append(row['Password'])
-
-    # Now, you can work with the email and password lists
-    for email, password in zip(emails, passwords):
-        print(f"Email: {email}, Password: {password}")
+    # Read the first row from the CSV
+    first_row = next(reader, None)
     
-        # Define the proxy server URL
-        proxy_server = "http://34.82.224.175:33333"
+    if first_row:
+        em = "cojikiw933@qianhost.com"
+        ps = "Hello@2413#"
 
-        # Set up Chrome options with the proxy server
-        chrome_options = webdriver.ChromeOptions()
-        # chrome_options = add_extension()
-        chrome_options.add_argument(f'--proxy-server={proxy_server}')
+# Check if data was found and print or use it
+if em is not None and ps is not None:
+    print(f"First Email: {em}, Password: {ps}")
+else:
+    print("No data found in the CSV.")
 
-        # Create the WebDriver instance with the Chrome options
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=chrome_options)
-        print('1')
+pyautogui.click(1019,223)
+# Select the text in the text box (you might need to adjust the number of times Ctrl+A is pressed)
+pyautogui.hotkey('ctrl', 'a')
 
-        driver.get("https://www.tiktok.com/en/")
-        print(driver.title)
-        time.sleep(8)
+# Press the "Delete" key to clear the text
+pyautogui.press('delete')
+sleep(5)
 
-        #testing
-        # time.sleep(5)
-        search_bar = driver.find_element(By.CLASS_NAME,"e1b6crsh1")
-        search_bar.click()
-        print("signup")
 
-        time.sleep(5)
-        
-        # search_bar = driver.find_elements(By.CLASS_NAME,"e1cgu1qo0")
-        # print(len(search_bar))
-        search_bar = driver.find_element(By.XPATH,"/html/body/div[7]/div[3]/div/div/div[1]/div[1]/div/div/div[2]/div[3]/p")
-        
-        # search_bar = driver.find_element(By.XPATH,"/html/body/div[7]/div[3]/div/div/div[1]/div[1]/div/div/div[2]/div[2]/div/svg")
-        # search_bar = driver.find_element(By.CSS_SELECTOR,"#loginContainer > div > div > div:nth-child(3) > div:nth-child(3) > p")
-        
-        search_bar.click()
-        print("click email")
-        
-        time.sleep(10)
-        # window_handles[1] is a second window 
-        driver.switch_to.window(driver.window_handles[1]) 
-        
-        # prints the title of the second window 
-        print("Second window title = " + driver.title)
-        
-        for i in email:
-            # print(i)
-            # driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div[2]/div/c-wiz/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input").send_keys(i)
-            driver.find_element(By.XPATH, "/html/body/div/div[2]/div[1]/form/div/div[1]/div/input").send_keys(i)
-            time.sleep(2)
+pyautogui.write(em)
+sleep(5)
+pyautogui.click(886,277)
+sleep(5)
+pyautogui.write(ps)
+sleep(5)
+pyautogui.click(892,329)
 
-        
-        for i in password:
-            # print(i)
-            driver.find_element(By.XPATH, "/html/body/div/div[2]/div[1]/form/div/div[2]/div/input").send_keys(i)
-            time.sleep(2)
-        
-        driver.find_element(By.XPATH,"/html/body/div/div[2]/div[1]/form/div/div[3]/label[2]").click()
-        time.sleep(10)
-
-        # window_handles[0] is a first window 
-        driver.switch_to.window(driver.window_handles[0])
-        print("coming back to first to window title = " + driver.title)
-        time.sleep(20)
-
-        # click on upload video
-        driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/div/div[3]/div[1]/a").click()
-        time.sleep(5)
-
-        # Now upload dummy videos
-        
-        driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div/div[3]/div[1]/a").click()
-        time.sleep(5)
-
-        driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div/div/div/div/input").send_keys(r'C:\Users\user\Desktop\PROJECTS\Multiple email\demo.mp4')
-        
-        #add caption
-        driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div/div[2]/div[2]/div[1]/div/div[1]/div[2]/div/div/div/div/div/div").send_keys('CAPTIONS')
-        
-        #post videos
-        driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div[2]/div[2]/div[8]/div[2]/button").click()
-
-        # quit driver 
-        driver.quit()
-
-if __name__ == '__main__':
-    main()
